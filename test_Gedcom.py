7d965cc5574c082d3934598f07d874112c94e25e
Prateek Menon <menon.patrick8@gmail.com>
2018-10-21 15:03:20 -0400

    Added US 28

--- test_Gedcom.py
+++ test_Gedcom.py
@@ -185,13 +185,21 @@ class TestGedcomParser(unittest.TestCase):
         ged.parse(file_path)
         checked_results = ged.check_siblings_count()
         self._check_ground_truth(checked_results, ground_truth_file_path)
-        
+
     #Testcase US 21
     def test_check_Correct_gender(self, file_path='test_files/Family.ged',
                                     ground_truth_file_path='test_files/testcase_21.json'):
         ged = Gedcom()
         ged.parse(file_path)
         checked_results = ged.check_Correct_gender()
+        self._check_ground_truth(checked_results, ground_truth_file_path)
+
+    # Testcase US 28
+    def test_order_siblings(self, file_path='test_files/Family.ged',
+                                  ground_truth_file_path='test_files/testcase_28.json'):
+        ged = Gedcom()
+        ged.parse(file_path)
+        checked_results = ged.order_siblings_by_age()
         self._check_ground_truth(checked_results, ground_truth_file_path)
 
 if __name__ == "__main__":
